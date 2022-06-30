5dc5818751d2b2dd7c47b3aa657ad6f1f6f9dce8
BLACK x Scripts <entetente187@gmail.com>
2022-06-30 15:43:28 +0200

    [ESX] BLACK-DOCS
    This is my first script. there may be individual bugs

---
+++ [ESX] BLACK-DOCS/BLACK_DOCS/main.lua
@@ -0,0 +1,65 @@
+--ESX Boilerplate
+ 
+ESX = nil
+local PlayerData = {}
+ 
+Citizen.CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(0)
+    end
+ 
+    while ESX.GetPlayerData().job == nil do
+        Citizen.Wait(10)
+    end
+ 
+    ESX.PlayerData = ESX.GetPlayerData();
+end)
+ 
+--Register Net Events
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+    PlayerData = xPlayer
+end)
+ 
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+        PlayerData.job = job
+end)
+
+local showUi = false
+
+Citizen.CreateThread(function()
+        Citizen.Wait(1)
+        while true do
+            if IsControlJustReleased(0, 344) then 
+                if showUi == false then 
+                    if ESX.PlayerData.job.name ~= nil and ESX.PlayerData.job.name == 'police' then --[!] Job
+                    SetNuiFocus(true, true)
+                    SendNUIMessage({type = "enable"})
+                    showUi = true;
+                end
+            end
+        end
+        Citizen.Wait(1)
+    end
+end)
+
+RegisterNUICallback('NUIFocusOff', function()
+    if showUi == true then 
+        SetNuiFocus(false, false)
+        SendNUIMessage({type = "disable"})
+        showUi = false
+    end
+end)
+
+print(" _____________________ ")
+print("|  _________________  |")
+print("| | BLACK x Scripts | |")
+print("| |_________________| |")
+print("|_____________________|")
+print(" ___________________________ ")
+print("|  _______________________  |")
+print("| | discord.gg/Sdsnu6HfKA | |")
+print("| |_______________________| |")
+print("|___________________________|")
---
+++ [ESX] BLACK-DOCS/README.txt
@@ -0,0 +1,83 @@
+[DE]
+
+ 
+
+Dies Ist Ein Simples Script Das Ermöglicht Google Dateien Ingame anzuzeigen.
+
+ 
+
+Was wird Unterstützt?
+- Google Docs
+- Google Tabellen (Am Besten Geeignet)
+- Google Formulare
+- Google Präsentation (Nicht Getestet)
+
+ 
+
+Installation:
+
+1.  "BLACK_DOCS" In den resources Ordner einfügen
+
+2. Einen der oben genannten Google diensten auswählen und einen ÖFFENDLICHEN Link erstellen
+
+3. Den Link dann in der "index.html" in Zeile NR: 7 Einfügen
+
+4. Start BLACK_DOCS In Die Server.zfg Eintragen 
+
+5. Server Restarten Und fertig
+
+ 
+
+Konfiguration:
+
+Job Ändern: main.lua > z.37 > police > Wunsch Job
+Größe Ändern: style.css > anpassen 
+
+ 
+
+Extra:
+- Alle Sprachen 
+- Nur ESX
+
+===================================================================================================
+
+[EN]
+
+ 
+
+This Is A Simple Script That Allows To Display Google Files Ingame.
+
+ 
+
+What is Supported?
+- Google Docs
+- Google Sheets (Best Suitable)
+- Google Forms
+- Google Slides (Not Tested)
+
+ 
+
+Installation:
+
+1. Paste "BLACK_DOCS" into the resources folder
+
+2. Select one of the above Google services and create a PUBLIC link
+
+3. Then insert the link in line NR: 7 of "index.html".
+
+4. Start BLACK_DOCS Enter in the Server.zfg
+
+5. Restart server And that's it
+
+ 
+
+Configuration:
+
+Change job: main.lua > z.37 > police > desired job
+Resize: style.css > customize
+
+ 
+
+Extra:
+- All languages
+- ESX only
